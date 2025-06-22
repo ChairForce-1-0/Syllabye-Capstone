5d6287321cf406284cd7e3293d0900619d868f4c
Ethan Cala <156372497+ethancala@users.noreply.github.com>
2025-06-21 20:09:26 -0500

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -15,7 +15,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrJvJyPIZW-0LiAe7jGU9GCCtJ4T9RDFI",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "syllabye-d56d5.firebaseapp.com",
   projectId: "syllabye-d56d5",
   storageBucket: "syllabye-d56d5.firebasestorage.app",
